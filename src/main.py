d124204da08824423377b205670a9e36a27e6486
ArengDev <kktdell.1@gmail.com>
2024-10-27 02:35:43 +0700

    added onpressed color change

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ brain = Brain()
 buttonlist = set()
 
 class Button:
-    def __init__(self, x, y, width, height, label="Button", color=Color.GREEN, callback=None, enable_hold=False):
+    def __init__(self, x, y, width, height, label="Button", color=Color.GREEN, callback=None, **kwargs):
         self.x = x
         self.y = y
         self.width = width
@@ -25,25 +25,31 @@ class Button:
         self.label = label
         self.color = color
         self.callback = callback
-        self.enable_hold = enable_hold
-        self.is_pressed_now = False
+        self.enable_hold = kwargs.get("enable_hold")
+        self.on_pressed_color = kwargs.get("on_pressed_color", Color.WHITE)
 
+        self.is_pressed_now = False
         self.draw()
 
         buttonlist.add(self)
 
-    def draw(self):
-        """Draw the button on the screen with centered text."""
-        brain.screen.set_fill_color(self.color)
-        brain.screen.set_pen_color(self.color)
+    def draw(self, pressed=False):
+        """Draw the button with visual feedback if pressed."""
+        # Use the on_pressed_color if pressed; otherwise, use the default color
+        fill_color = self.on_pressed_color if pressed else self.color
+
+        brain.screen.set_fill_color(fill_color)
         brain.screen.draw_rectangle(self.x, self.y, self.width, self.height)
 
+        # Center the label text
         text_width = len(self.label) * 6
         text_x = self.x + (self.width - text_width) // 2 - 4
-        text_y = self.y + (self.height - 8) // 2  # Center text vertically
+        text_y = self.y + (self.height - 8) // 2
 
         brain.screen.set_pen_color(Color.BLACK)
         brain.screen.print_at(self.label, x=text_x, y=text_y)
+        brain.screen.set_fill_color(Color.TRANSPARENT)
+        brain.screen.set_pen_color(Color.WHITE)
 
     def is_pressed(self, touch_x, touch_y):
         """Check if the button is pressed based on touch coordinates."""
@@ -53,14 +59,16 @@ class Button:
     def handle_press(self):
         """Trigger the callback if the button is pressed."""
         if self.enable_hold or not self.is_pressed_now:
+            self.draw(pressed=True)
             if self.callback:
                 self.callback()
             self.is_pressed_now = True
 
     def reset(self):
         """Reset the button state when released."""
-        self.is_pressed_now = False
-
+        if self.is_pressed_now:
+            self.draw(pressed=False)
+            self.is_pressed_now = False
     def delete(self):
         """Remove the button visually and from the listener."""
         brain.screen.set_fill_color(Color.BLACK)
